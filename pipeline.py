02341e965dcc0d0ca9be541408adf2c70863eef8
Ammar <muhammad2000ammar@gmail.com>
2024-02-17 18:27:57 +0500

    added requirements.txt file

--- pipeline.py
+++ pipeline.py
@@ -3,3 +3,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-0.5B")
 model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen1.5-0.5B")
+
+answer = model("tell me about the charter of democracy")
+print(answer)
