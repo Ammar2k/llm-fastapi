5c57bf57327f2d6d19924c50d510f8ba19638426
Ammar <muhammad2000ammar@gmail.com>
2024-02-18 19:24:28 +0500

    used pipeline to generate response

--- pipeline.py
+++ pipeline.py
@@ -1,32 +1,40 @@
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForCausalLM
+# Use a pipeline as a high-level helper
+from transformers import pipeline
 
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-0.5B")
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen1.5-0.5B")
+
+# tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-0.5B")
+# model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen1.5-0.5B")
 
 prompt = "tell me about the charter of democracy in Pakistani politics."
 
-messages = [
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": prompt}
-]
+# messages = [
+#     {"role": "system", "content": "You are a helpful assistant."},
+#     {"role": "user", "content": prompt}
+# ]
 
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True
-)
+# text = tokenizer.apply_chat_template(
+#     messages,
+#     tokenize=False,
+#     add_generation_prompt=True
+# )
 
-model_inputs = tokenizer([text], return_tensors="pt")
+# model_inputs = tokenizer([text], return_tensors="pt")
 
-generated_ids = model.generate(
-    model_inputs.input_ids,
-    max_new_tokens=512
-)
+# generated_ids = model.generate(
+#     model_inputs.input_ids,
+#     max_new_tokens=512
+# )
 
-generated_ids = [
-    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-]
+# generated_ids = [
+#     output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+# ]
 
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(response)
+# response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+# print(response)
+
+pipe = pipeline("text-generation", model="Qwen/Qwen1.5-0.5B")
+
+answer = pipe(prompt)
+print(answer)
